4d5d7ec2caadad64e5219489d0226d5447ed731d
TheSProyect <jesusecova73@gmail.com>
2025-05-08 15:25:50 -0400

    perfil con información dinámica

--- js/perfil.js
+++ js/perfil.js
@@ -1,7 +1,38 @@
 window.onload = function () {
+    let params = new URLSearchParams(window.location.search);
+    let ci = params.get("ci");
+
+    let script = document.createElement('script');
+    script.src = `${ci}/perfil.json`;
+    script.onload = function () {
+		
+        document.title = perfil.nombre;
+        document.getElementById("foto").src = `${ci}/${ci}.jpg`;
+        document.getElementById("titulo").textContent = perfil.nombre;
+        document.getElementById("descripcion").textContent = perfil.descripcion;
+        document.getElementById("colorFav").textContent = perfil.color;
+        document.getElementById("bookFav").textContent = perfil.libro;
+        document.getElementById("musicFav").textContent = perfil.musica;
+        document.getElementById("gameFav").textContent = perfil.video_juego;
+        document.getElementById("learnLang").innerHTML = perfil.lenguajes.map(l => `<strong>${l}</strong>`).join(', ');
+        
+        let emailLink = document.getElementById("mail");
+        emailLink.href = `mailto:${perfil.email}`;
+        emailLink.textContent = perfil.email;
+
+    };
+
+    script.onerror = function () {
+        document.body.textContent = `Error al cargar`;
+    };
+
+    document.body.appendChild(script);
+	
+	
+	let lang = (params.get("lang") || "ES").toUpperCase(); 
 	
 	let configScript = document.createElement("script");
-	configScript.src = `conf/configES.json`;
+	configScript.src = `conf/config${lang}.json`;
 	
 	configScript.onload = function () {
 		document.getElementById("colorPrompt").textContent = config.color;
@@ -10,7 +41,7 @@ window.onload = function () {
 		document.getElementById("gamePrompt").textContent = config.video_juego;
 		document.getElementById("langPrompt").innerHTML = `<strong>${config.lenguajes}</strong>`;
 		
-		document.getElementById("mailPrompt").innerHTML = config.email.replace("[email]", `<a href="mailto:samuelenriquefr@gmail.com">samuelenriquefr@gmail.com</a>`);
+		document.getElementById("mailPrompt").innerHTML = config.email.replace("[email]", `<a href="mailto:${perfil.email}">${perfil.email}</a>`);
 		
 	}
 	
